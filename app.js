044336d8fb9378a41803ba8a733d2655f6da79e0
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-13 17:20:53 -0500

    Added functionality to reduce the image size maintaining the aspect ratio.

---
+++ app.js
@@ -0,0 +1,43 @@
+function process() {
+
+    const file = document.querySelector('#upload').files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = (event) => {
+
+        const imgElement = document.createElement('img');
+
+        imgElement.src = event.target.result;
+
+        document.querySelector('#input').src = event.target.result;
+
+        imgElement.onload = (e) => {
+
+            const canvas = document.createElement('canvas');
+
+            const MAX_WIDTH = 400;
+
+            const scaleSize = MAX_WIDTH / e.target.width;
+
+            canvas.width = MAX_WIDTH;
+
+            canvas.height = e.target.height * scaleSize;
+
+            const ctx = canvas.getContext("2d");
+
+            ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+
+            const srcEncoded = ctx.canvas.toDataURL(e.target, "image/jpg");
+
+            document.querySelector('#output').src = srcEncoded;
+
+        }
+
+    }
+
+}
